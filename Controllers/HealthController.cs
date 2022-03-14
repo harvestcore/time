bc7f9240dbd28c76cc95bed6232548499ce96b92
Ángel Gómez <aagomezies@gmail.com>
2022-03-14 20:59:43 +0100

    Add health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestApp.Controllers
+{
+    public class Status
+    {
+        public bool Ok { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Status()
+            {
+                Ok = true,
+            });
+        }
+    }
+}
